09179caee492d8e89e46e3c63977266a733dddc9
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2025-04-24 12:53:14 -0700

    Finished the practice, just gotta add some comments later.

--- usestate/src/App.jsx
+++ usestate/src/App.jsx
@@ -1,11 +1,14 @@
 import RestaurantsContainer from "./components/RestaurantsContainer";
 import { restaurants } from "./data/data.js";
+import { useState } from 'react';
 import "./App.css";
 
 function App() {
+  const [restaurantState, setRestaurants] = useState([...restaurants]);
+  
   return (
     <div className="App">
-      <RestaurantsContainer />
+      <RestaurantsContainer restaurants={restaurantState} />
     </div>
   );
 }
--- usestate/src/components/Restaurant.jsx
+++ usestate/src/components/Restaurant.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
 function Restaurant(props) {
+  const [hours, setHours] = useState(false);
+  const [menu, setMenu] = useState(false);
+
+  const handleHours = () => {
+    setHours((prev) => !prev);
+  };
+  const handleMenu = () => {
+    setMenu((prev) => !prev);
+  };
+
   return (
     <div className="restaurant">
       <img src={props.restaurant.image} alt={props.name} />
@@ -9,10 +20,28 @@ function Restaurant(props) {
         <li>{props.restaurant.cuisine}</li>
         <li>{props.restaurant.rating}</li>
       </ul>
-      <button>Hours</button>
-      <button>Menu</button>
+      <button onClick={handleHours}>Hours</button>
+      <button onClick={handleMenu}>Menu</button>
+      {menu && (
+        <ul>
+          {props.restaurant.menu.map((menuItem, index) => (
+            <li key={index}>{`${menuItem.item} $${menuItem.price}`}</li>
+          ))}
+        </ul>
+      )}
+      {hours && (
+        <ul>
+          {Object.entries(props.restaurant.hours).map(([day, time]) => (
+            <li key={day}>
+              <strong>{day.charAt(0).toUpperCase() + day.slice(1)}:</strong>{" "}
+              {time}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
 
 export default Restaurant;
+
